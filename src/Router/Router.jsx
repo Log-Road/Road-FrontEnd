5bbe7f9b8805d6b7e0d9873881746f5e8d90d4c5
hoseon07 <a01081930429@gmail.com>
2024-03-08 16:18:18 +0900

    💄:: 메인페이지 퍼블리싱

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RankingPage from "../pages/RankingPage"
+import Main from "../pages/Main";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/main" element={<Main />} />
         <Route path="/ranking" element={<RankingPage/>}/>
       </Routes>
     </BrowserRouter>
