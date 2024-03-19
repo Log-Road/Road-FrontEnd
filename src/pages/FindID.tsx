19a4e0ac98869defa37de7e295cd962bcb73e524
limda00 <dylim7715@gmail.com>
2024-03-20 00:37:58 +0900

    ✨ :: 버튼 색 활성화 추가

--- src/pages/FindID.tsx
+++ src/pages/FindID.tsx
@@ -14,6 +14,7 @@ export default function FindID() {
 
     const [email, setEmail] = useState<String>("")
     const [AuthenticationNumber, setAuthenticationNumber] = useState<String>("")
+    const [possibleLogin, setPossibleLogin] = useState<boolean>(false)
 
     return (
         <Container>
@@ -33,7 +34,7 @@ export default function FindID() {
                 </Wrap>
 
                 <BtnWrap>
-                    <LoginButton onClick={() => { navigate("/login") }}>로그인</LoginButton>
+                    <LoginButton possibleLogin={possibleLogin} Click={() => { navigate("/login") }}>로그인</LoginButton>
                     <HandlePassword onClick={() => {navigate("/findPassWord")}}>비밀번호 찾기</HandlePassword>
                 </BtnWrap>
 
@@ -94,8 +95,8 @@ width: 22.5em;
 height: 3.25em;
 border: none;
 border-radius: 0.75em;
-background-color: ${colors.Main};
-color: #fff;
+background-color: ${({ possibleLogin }) => possibleLogin ? colors.Main : colors.Gray["gray 200"]};
+color: ${({ possibleLogin }) => (possibleLogin ? "#fff" : colors.gray2)};
 display: flex;
 justify-content: center;
 align-items: center;
@@ -103,7 +104,7 @@ font-family: 'Pretendard-Medium';
 font-size: 1em;
 
 &:hover {
-    background-color: ${colors.Blue["main 600"]};
+    background-color: ${({ possibleLogin }) => possibleLogin ? colors.Blue["main 600"] : colors.Gray["gray 300"]};
 }
 `
 
