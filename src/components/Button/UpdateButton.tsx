b328f4528ced6bb7a4c2133c8fc2188a52ec5d60
lim58 <dylim7715@gmail.com>
2024-04-07 21:11:48 +0900

    💄 :: 수정 버튼 컴포넌트 생성

---
+++ src/components/Button/UpdateButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import {colors} from "../../styles/colors"
+
+export default function UpdateButton() {
+    return (
+        <>
+            <Button>수정</Button>
+        </>
+    )
+}
+
+const Button = styled.button`
+width: 4em;
+height: 2em;
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: ${colors.White};
+border: 0.1em solid ${colors.gray1};
+border-radius: 0.63em;
+color: ${colors.gray2};
+font-family: 'Pretendard-Regular';
+font-size: 0.85em;
+
+&:hover {
+    background-color: ${colors.Gray["gray 50"]};
+}
+`
