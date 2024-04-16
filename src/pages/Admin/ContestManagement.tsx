b82d45f21f7cfa9ae98ff25db02d4a751bb3bf60
lim58 <dylim7715@gmail.com>
2024-04-16 22:05:54 +0900

    ♻️ :: 대회 관리 페이지 자잘한 수정

--- src/pages/Admin/ContestManagement.tsx
+++ src/pages/Admin/ContestManagement.tsx
@@ -5,25 +5,25 @@ import PlusButton from "../../components/Button/PlusButton";
 import HandleButton from "../../components/Button/HandleButton";
 import { colors } from "../../styles/colors";
 
-interface Props{
-  state: string;
-}
-
 /**
  * 
  * @returns 대회 관리 리스트 페이지
  */
 
+interface Props{
+  state: string;
+}
+
 export default function ContestManagement() {
 
-    const ContestData = [ //백엔드에서 받아올 값
+    const ContestData = [ //백엔드에서 받아올 값 (임시로 값 지정함)
         { title: "뭐시깽이 대회", startDate: "2023.3.14", overDate: "2023.04.14", state: "진행중" },
         { title: "무슨무슨 대회", startDate: "2023.2.14", overDate: "2023.03.14", state: "시상 대기" },
         { title: "해커톤", startDate: "2023.2.14", overDate: "2023.03.14", state: "시상 완료" },
     ];
 
     const showButton = (state) => { //대회 상태 (진행중/대기/완료)에 따라 나타나는 버튼 지정
-      if(state === "진행중") return <><HandleButton text="글쓰기" /><HandleButton text="수정" /></>;
+      if(state === "진행중") return <><HandleButton text="글보기" /><HandleButton text="수정" /></>;
       else if(state === "시상 대기") return <><HandleButton text="글보기" /><HandleButton text="시상" /></>;
       else if(state === "시상 완료") return <HandleButton text="결과" />;
     } 
@@ -43,7 +43,7 @@ export default function ContestManagement() {
                     <InfoWrap>
 
                         {ContestData.map((value, index) => (
-                            <ContestWrap>
+                            <ContestWrap key={index}>
                                 <FlexWrap>
                                     <Name>{value.title}</Name>
                                     <Date>{value.startDate} ~ {value.overDate}</Date>
@@ -65,10 +65,7 @@ export default function ContestManagement() {
 
 const Container = styled.div`
   width: 100vw;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 1.25em;
+  height: 100vh;
 `;
 
 const InnerContainer = styled.div`
@@ -76,7 +73,6 @@ const InnerContainer = styled.div`
   height: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
 `;
 
 const Contents = styled.div`
