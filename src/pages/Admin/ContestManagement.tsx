9c18d29d6a858fb3d4776cc0231970785f3b06f5
lim58 <dylim7715@gmail.com>
2024-04-21 20:04:27 +0900

    💄 :: 콘텐츠 크기 조정

--- src/pages/Admin/ContestManagement.tsx
+++ src/pages/Admin/ContestManagement.tsx
@@ -11,59 +11,60 @@ import { useNavigate } from "react-router-dom";
  * @returns 대회 관리 리스트 페이지
  */
 
-interface Props{
+interface Props {
   state: string;
 }
 
 export default function ContestManagement() {
 
-    const native = useNavigate()
+  const native = useNavigate()
 
-    const ContestData = [ //백엔드에서 받아올 값 (임시로 값 지정함)
-        { title: "뭐시깽이 대회", startDate: "2023.3.14", overDate: "2023.04.14", state: "진행중" },
-        { title: "무슨무슨 대회", startDate: "2023.2.14", overDate: "2023.03.14", state: "시상 대기" },
-        { title: "해커톤", startDate: "2023.2.14", overDate: "2023.03.14", state: "시상 완료" },
-    ];
+  const ContestData = [ //백엔드에서 받아올 값 (임시로 값 지정함)
+    { title: "뭐시깽이 대회", startDate: "2023.3.14", overDate: "2023.04.14", state: "진행중" },
+    { title: "무슨무슨 대회", startDate: "2023.2.14", overDate: "2023.03.14", state: "시상 대기" },
+    { title: "해커톤", startDate: "2023.2.14", overDate: "2023.03.14", state: "시상 완료" },
+  ];
 
-    const showButton = (state) => { //대회 상태 (진행중/대기/완료)에 따라 나타나는 버튼 지정
-      if(state === "진행중") return <><HandleButton text="글보기" onClick={() => native("/")}/><HandleButton text="수정" onClick={() => native("/")}/></>;
-      else if(state === "시상 대기") return <><HandleButton text="글보기" onClick={() => native("/")}/><HandleButton text="시상" onClick={() => native("/")}/></>;
-      else if(state === "시상 완료") return <HandleButton text="결과" onClick={() => native("/")}/>;
-    } 
+  const showButton = (state) => { //대회 상태 (진행중/대기/완료)에 따라 나타나는 버튼 지정
+    if (state === "진행중") return <><HandleButton text="글보기" onClick={() => native("/")} /><HandleButton text="수정" onClick={() => native("/")} /></>;
+    else if (state === "시상 대기") return <><HandleButton text="글보기" onClick={() => native("/")} /><HandleButton text="시상" onClick={() => native("/")} /></>;
+    else if (state === "시상 완료") return <HandleButton text="결과" onClick={() => native("/")} />;
+  }
 
-    return (
-        <Container>
-            <Header />
+  return (
+    <Container>
+      <Header />
 
-            <InnerContainer>
-                <Contents>
+      <InnerContainer>
+        <Contents>
 
-                    <TopWrap>
-                        <Title>대회 관리</Title>
-                        <PlusButton text="대회 추가" onClick={() => native("/")}/>
-                    </TopWrap>
+          <TopWrap>
+            <Title>대회 관리</Title>
+            <PlusButton text="대회 추가" onClick={() => native("/")} />
+          </TopWrap>
 
-                    <InfoWrap>
+          <InfoWrap>
 
-                        {ContestData.map((value, index) => (
-                            <ContestWrap key={index}>
-                                <FlexWrap>
-                                    <Name>{value.title}</Name>
-                                    <Date>{value.startDate} ~ {value.overDate}</Date>
-                                    <State state={value.state}>{value.state}</State>
-                                </FlexWrap>
-                                <FlexWrap>
-                                    {showButton(value.state)}
-                                </FlexWrap>
-                            </ContestWrap>
-                        ))}
+            {ContestData.map((value, index) => (
+              <ContestWrap key={index}>
+                <FlexWrap>
+                  <Name>{value.title}</Name>
+                  <Date>{value.startDate} ~ {value.overDate}</Date>
+                  <State state={value.state}>{value.state}</State>
+                </FlexWrap>
+                
+                <FlexWrap>
+                  {showButton(value.state)}
+                </FlexWrap>
+              </ContestWrap>
+            ))}
 
-                    </InfoWrap>
+          </InfoWrap>
 
-                </Contents>
-            </InnerContainer>
-        </Container>
-    );
+        </Contents>
+      </InnerContainer>
+    </Container>
+  );
 }
 
 const Container = styled.div`
@@ -79,7 +80,6 @@ const InnerContainer = styled.div`
 `;
 
 const Contents = styled.div`
-  width: 40%;
   display: flex;
   flex-direction: column;
   gap: 28px;
@@ -98,6 +98,10 @@ const InfoWrap = styled.div`
 `;
 
 const ContestWrap = styled.div`
+width: 600px;
+@media (max-width: 700px) and (min-width: 100px) {
+    width: 500px;
+}
 display: flex;
 justify-content: space-between;
 align-items: center;
